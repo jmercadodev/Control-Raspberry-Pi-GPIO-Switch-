8c74092b2c543e355e933f30bd380f9ec00e5ae1
Jonathan Mercado <jonathanmercado.dev@gmail.com>
2018-05-31 00:27:25 -0500

    Create fan_switch.py

---
+++ fan_switch.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as gpio
+import time
+import os
+
+gpio.setmode(gpio.BCM)
+gpio.setup(4, gpio.OUT)
+
+while True:
+    temp = os.popen('vcgencmd measure_temp').readline()
+    threshold = int(35)
+    temp = int(temp[5:7])
+##    print(temp)
+    
+    if temp >= threshold:
+        print('Fan On | {} >= {}' .format(temp, threshold))
+        gpio.output(4,True)
+        time.sleep(3)
+
+    else:
+        print('Fan OFF | {} <= {}' .format(temp, threshold))
+        gpio.output(4, False)
+        time.sleep(3)
